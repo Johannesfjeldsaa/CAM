cfe0d73ad387a4dec69d31989d35efc3194218f4
Øyvind Seland <oyvinds@login-2.betzy>
2025-06-24 17:11:21 +0200

    Included sulfater aitken mode in the potential number of IN as done in CESM3; Turned off BC immersion freezing by setting logical variable do_bc equal false everywhere

--- src/oslo_aero_hetfrz.F90
+++ src/oslo_aero_hetfrz.F90
@@ -1108,12 +1108,12 @@ contains
 
     ! if aw*Si<=1, the freezing point depression is strong enough to prevent freezing
 
-    if (aw(id_bc)*supersatice > 1._r8 ) then
-       do_bc   = .true.
-       rgimm_bc = 2*vwice*sigma_iw/(kboltz*t*LOG(aw(id_bc)*supersatice))
-    else
+!    if (aw(id_bc)*supersatice > 1._r8 ) then
+!       do_bc   = .true.
+!       rgimm_bc = 2*vwice*sigma_iw/(kboltz*t*LOG(aw(id_bc)*supersatice))
+!    else
        do_bc = .false.
-    end if
+!    end if
 
     if (aw(id_dst1)*supersatice > 1._r8 ) then
        do_dst1 = .true.
